8d07205444c0eca4674f4a42795bc696f25a0cca
Mauricio <mauricio.mantilla.ruales@gmail.com>
2024-10-21 13:30:34 -0500

    Avance del ejercicio4 de maps

---
+++ src/Practice/Clase5/Ejercicio4/Main.java
@@ -0,0 +1,16 @@
+package Practice.Clase5.Ejercicio4;
+
+import ADT.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeMap<Integer,String> map = new TreeMap<>();
+        map.put(1,"A");
+        map.put(2,"B");
+        map.put(3,"C");
+        map.put(4,"D");
+        map.put(5,"E");
+
+
+    }
+}
