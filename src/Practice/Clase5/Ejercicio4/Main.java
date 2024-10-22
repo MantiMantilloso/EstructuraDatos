fe2d28fc27609feaa898827d84528ab7035959f2
Mauricio <mauricio.mantilla.ruales@gmail.com>
2024-10-21 20:19:39 -0500

    Ejercicio de arbol, buscar y entender el balanceo

--- src/Practice/Clase5/Ejercicio4/Main.java
+++ src/Practice/Clase5/Ejercicio4/Main.java
@@ -1,9 +1,18 @@
 package Practice.Clase5.Ejercicio4;
 
+import ADT.Entry;
+import ADT.Position;
 import ADT.TreeMap;
 
 public class Main {
+    public static class ArbolEjemplo<Integer,String> extends TreeMap<Integer,String>{
+        public void balancetree(Position<Entry<Integer,String>> center){
+            restructure(center);
+        }
+    }
     public static void main(String[] args) {
+
+
         TreeMap<Integer,String> map = new TreeMap<>();
         map.put(1,"A");
         map.put(2,"B");
@@ -11,6 +20,15 @@ public class Main {
         map.put(4,"D");
         map.put(5,"E");
 
+        ArbolEjemplo<Integer, String> arbol = new ArbolEjemplo<>();
+        arbol.put(1,"A");
+        arbol.put(2,"B");
+        arbol.put(3,"C");
+        arbol.put(4,"D");
+        arbol.put(5,"E");
+
+        Position<Entry<Integer,String>> root = arbol.root();
+        arbol.balancetree(root);
 
     }
 }
