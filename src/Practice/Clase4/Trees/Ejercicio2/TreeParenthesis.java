437a7f559797ef3efbbc91df01f59273ca506112
Mauricio <mauricio.mantilla.ruales@gmail.com>
2024-10-22 10:38:14 -0500

    Usando el codigo proporcionado, se optimizo la funcion parentesisExpresion

---
+++ src/Practice/Clase4/Trees/Ejercicio2/TreeParenthesis.java
@@ -0,0 +1,25 @@
+package Practice.Clase4.Trees.Ejercicio2;
+
+import ADT.LinkedTree;
+import ADT.Position;
+
+public class TreeParenthesis<E> extends LinkedTree<E> {
+
+    public String parentesisExpresion(Position<E> p, int indent){
+        Node<E> node = (Node<E>) p;
+        StringBuilder expression = new StringBuilder();
+        expression.append("\t".repeat(Math.max(0, indent)));
+        if (this.isInternal(node)){
+            expression.append(node.getElement().toString()).append("(\n");
+            indent++;
+            for(Node<E> o : node.getChildren()){
+                expression.append(parentesisExpresion(o, indent + 1));
+            }
+            indent--;
+            expression.append("\t".repeat(Math.max(0, indent)));
+            expression.append(")\n");
+        }else
+            expression.append(node.getElement().toString()).append("\n");
+        return expression.toString();
+    }
+}
